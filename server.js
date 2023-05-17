7e87bf330ade1320bd80ceb12b698fc9451020ba
Christopher Munoz <christopherm0507@gmail.com>
2023-05-17 00:38:31 -0400

    Changes for deployment

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-
-require('dotenv').config({path: '.env'})
-
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
